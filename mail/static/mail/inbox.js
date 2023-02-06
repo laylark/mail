fe6d4c0f2338fd6bb0868eb9cc4959139b52142f
laylark <laylan@gmail.com>
2023-02-05 22:49:19 -0600

    Add post request for form submission and display emails

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -20,6 +20,36 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+  
+  // Listen for form submission
+  document.querySelector('#compose-form').onsubmit = () => {
+    const recipients = document.querySelector('#compose-recipients').value;
+    const subject = document.querySelector('#compose-subject').value;
+    const body = document.querySelector('#compose-body').value;
+
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients,
+          subject,
+          body,
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      if (result.error) {
+        // Add error message to view
+        console.log(result.error);
+      } else {
+        load_mailbox('sent');
+      }
+    })
+    .catch(error => {
+      console.log({error});
+    });
+
+    return false;
+  }
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +60,49 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  const container = document.createElement('div');
+  container.classList = 'container';
+
+  fetch('/emails/inbox')
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+    console.log(emails);
+
+    // ... do something else with emails ...
+
+    emails.forEach(email => {
+
+      const sender = email.sender;
+      const subject = email.subject;
+      const timestamp = email.timestamp;
+
+      const divRow = document.createElement('div');
+      divRow.classList.add('row', 'pb-2');
+      divRow.style.borderBottom = '2px solid';
+
+      const divSender = document.createElement('div');
+      divSender.classList = 'col-sm';
+      divSender.innerHTML = sender;
+
+      const divSubject = document.createElement('div');
+      divSubject.classList = 'col-sm';
+      divSubject.innerHTML = subject;
+
+      const divTimestamp = document.createElement('div');
+      divTimestamp.classList = 'col-sm';
+      divTimestamp.innerHTML = timestamp;
+
+      document.querySelector('#emails-view').append(container);
+      container.append(divRow);
+      divRow.append(divSender);
+      divRow.append(divSubject);
+      divRow.append(divTimestamp);
+
+      if (email.read == false) {
+      divRow.style.backgroundColor = '#D3D3D3';
+      }
+    });
+  })
 }
